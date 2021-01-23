b5c72a9702dfa2629cf0e37ef08c72114f134fc4
hanzhitao <hzt1169@sina.com>
2021-01-23 14:46:12 +0800

    快捷支付

--- pages/home/quickPay/index.vue
+++ pages/home/quickPay/index.vue
@@ -1,18 +1,50 @@
 <<template>
-	<view>
-		<view class="top-title align-center text-xxl  ly-font-color-6 padding-tb-xs ">
+	<view class="quick-pay">
+		<view class="solid"></view>
+		<view class=" align-center">
+		<view class="quickPay-bb width match-width align-center text-xl  ly-font-color-6 padding-tb-xs quick-pay-fw">
+			<!-- <text class="text-price">aaa</text> -->
 			确认付款
 		</view>
+		
+			<view class="quickPay-bb text-price width match-width align-center  text-xl  ly-font-color-6 padding-tb-xs quick-pay-fw">
+				200
+			</view>
+		
+			<view class="quickPay-bb  width match-width  text-lg   ly-font-color-6 padding-tb-xs quick-pay-fw">
+				<text class="">付款方式</text>
+				
+				<view class="fr arrow">
+					<image
+						class='match-parent'
+						src='../../../static/homev2/right.png'
+						mode='scaleToFill'
+					/>
+				</view>
+				<text class="fr margin-right-xs">兴业银行</text>
+			</view>
+		
+		</view>
+		
 	</view>
 </template>
 <script></script>
 <style lang="scss">
-	.top-title{
-		border:1upx solid #f1f1f3;
-		width:100%;
+	.quickPay-bb{
+		border-bottom:1upx solid #f1f1f3;
+	}
+	.quick-pay-fw{
 		font-weight:500;
-		// height:40upx;
-		// background-color:red;
-		
+	}
+	.width{
+		width: 90%;
+	}
+	.pr{
+		position: relative;
+	}
+	.arrow {
+		width: 14upx;
+		height: 25upx;
+		margin-top: 7upx;
 	}
 </style>
